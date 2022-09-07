3d247254159f786a78a93939bb92b3d6d25f0439
jelena <jelena@mindable.health>
2022-09-07 17:08:09 +0200

    added error page

---
+++ error.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="container">
+      <h1>Page not found</h1>
+      <NuxtLink to="/restaurants">Back to Restaurants</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  text-align: center;
+  margin-top: 5rem;
+}
+</style>
