25e8bf6b4f2303db71b3c7dcc66155f1dcd538de
jelena <jelena@mindable.health>
2022-09-07 20:08:57 +0200

    03-add-card-grid

---
+++ components/restaurant/Card.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+interface CardProps {
+  name: string;
+  img: string;
+  text: string;
+}
+
+const props = defineProps<CardProps>();
+</script>
+
+<template>
+  <div class="card">
+    <img class="card-img-top" :src="img" alt="Card image cap" />
+    <div class="card-body">
+      <h5 class="card-title">{{ name }}</h5>
+      <p class="card-text text-truncate">
+        {{ text }}
+      </p>
+      <a :href="`/restaurants/${name}`" class="btn btn-primary"
+        >Read about {{ name }}</a
+      >
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,18 @@
+<script setup lang="ts">
+import restaurants from "@/data.json";
+const { isDarkMode } = useDarkMode();
+</script>
+
 <template>
   <div>
     <div class="container">
       <h1>Welcome to Restaurantly</h1>
       <NuxtLink to="/restaurants">Restaurants</NuxtLink>
+      <div class="row">
+        <div v-for="r in restaurants" class="col-sm-4 mt-5 mb-5">
+          <RestaurantCard :name="r.name" :img="r.imageUrl" :text="r.content" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
