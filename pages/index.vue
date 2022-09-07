737a467487f47d59ae7076d1d9081ce025de80ae
jelena <jelena@mindable.health>
2022-09-07 17:10:10 +0200

    main page

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="container">
+      <h1>Welcome to Restaurantly</h1>
+      <NuxtLink to="/restaurants">Restaurants</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  text-align: center;
+  margin-top: 5rem;
+}
+</style>
